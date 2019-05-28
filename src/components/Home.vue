f616fbecdb49c70e575b01916841917460c6c24f
HYHYPan <sea39801991@gmail.com>
2019-05-28 17:06:22 +0800

    add the components of NavBar & Footer, finish the About part of home page.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,130 +1,14 @@
 <template>
   <div>
     <header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Carousel</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-          <form class="form-inline mt-2 mt-md-0">
-            <input
-              class="form-control mr-sm-2"
-              type="text"
-              placeholder="Search"
-              aria-label="Search"
-            >
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
+      <NavBar></NavBar> 
     </header>
 
-    <main role="main">
-      <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <svg
-              class="bd-placeholder-img"
-              width="100%"
-              height="100%"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-              role="img"
-            >
-              <rect width="100%" height="100%" fill="#777"></rect>
-            </svg>
-            <div class="container">
-              <div class="carousel-caption text-left">
-                <h1>Example headline.</h1>
-                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p>
-                  <a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <svg
-              class="bd-placeholder-img"
-              width="100%"
-              height="100%"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-              role="img"
-            >
-              <rect width="100%" height="100%" fill="#777"></rect>
-            </svg>
-            <div class="container">
-              <div class="carousel-caption">
-                <h1>Another example headline.</h1>
-                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p>
-                  <a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <svg
-              class="bd-placeholder-img"
-              width="100%"
-              height="100%"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-              role="img"
-            >
-              <rect width="100%" height="100%" fill="#777"></rect>
-            </svg>
-            <div class="container">
-              <div class="carousel-caption text-right">
-                <h1>One more for good measure.</h1>
-                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p>
-                  <a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a>
-                </p>
-              </div>
-            </div>
-          </div>
+    <main role="main ">
+      <div id="bigBanner">
+        <div class="bigBanner-inner">
+          <img :src="banner3" alt="Los Angeles" style="width:100%">
         </div>
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
       </div>
 
       <!-- Marketing messaging and featurettes
@@ -132,23 +16,66 @@
       <!-- Wrap the rest of the page in another container to center all the content. -->
 
       <div class="container marketing">
-        <!-- Three columns of text below the carousel -->
+        <div class="row featurette">
+          
+            <div class="col-md-7 order-md-2 ">
+              <h2 class="featurette-heading">
+                About Me
+                <span class="text-muted"></span>
+              </h2>
+
+              <h5 class="mt-4">
+                <i class="fas fa-female"></i> FEMALE
+              </h5>
+
+              <h5>
+                <i class="fas fa-walking"></i> PERSONAITY
+              </h5>
+
+              <ul>
+                <li>Have strong passions to innovated good ideas to help and communicate with people.</li>
+                <li>Willing to learn about new processes and concepts with a strong sense of curiosity.</li>
+                
+              </ul>
+
+              <h5>
+                <i class="fas fa-university"></i> EDUCATION
+              </h5>
+
+              <ul>
+                <li>
+                  National Cheng Chi University (NCCU)
+                  <ul>
+                    <li>Bachelor of Chinese Literature, Jun. 2013.</li>
+                    <li>Minor: Economics, Jun. 2013.</li>
+                  </ul>
+                </li>
+                <li>Taipei Municipal Datong High School</li>
+              </ul>
+            </div>
+
+            <div class="col-md-5 order-md-1 mt-4 about-img">
+              <img
+                :src="aboutImg"
+                alt="about-img"
+                class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto container"
+              >
+            </div>
+          
+        </div>
+
+        <hr class="featurette-divider mt-4">
+
+        <!-- The row start from here-->
         <div class="row">
           <div class="col-lg-4">
-            <svg
-              class="bd-placeholder-img rounded-circle"
-              width="140"
-              height="140"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-              role="img"
-              aria-label="Placeholder: 140x140"
-            >
-              <title>Placeholder</title>
-              <rect width="100%" height="100%" fill="#777"></rect>
-              <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-            </svg>
+            <title>Placeholder</title>
+
+            <rect width="100%" height="100%" fill="#777">
+              <img :src="logo1" alt="Logo1">
+            </rect>
+            <text x="50%" y="50%" fill="#77" dy=".3em">140x140</text>
+
             <h2>Heading</h2>
             <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
             <p>
@@ -302,16 +229,29 @@
       <!-- /.container -->
 
       <!-- FOOTER -->
-      <footer class="container">
-        <p class="float-right">
-          <a href="#">Back to top</a>
-        </p>
-        <p>
-          &copy; 2017-2019 Company, Inc. &middot;
-          <a href="#">Privacy</a> &middot;
-          <a href="#">Terms</a>
-        </p>
-      </footer>
+      <Footer></Footer>
     </main>
   </div>
 </template>
+
+<script>
+import NavBar from "./NavBar";
+import Footer from "./footer";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      logo1: "static/images/logo.png",
+      banner1: "static/images/banner01.png",
+      banner2: "static/images/banner02.png",
+      banner3: "static/images/banner03.png",
+      aboutImg: "static/images/head-2.jpg"
+    };
+  },
+  components:{
+      NavBar,
+      Footer,
+  }
+};
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+          integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+          crossorigin="anonymous"
+        >
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                HAZELPAN
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Work Experiences</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Skills</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    
+
+    
+  </div>
+</template>
+
