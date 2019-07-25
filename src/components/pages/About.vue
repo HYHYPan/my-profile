3346fb591597ca5b0d0a0b2d536ac11e31eb51e8
HYHYPan <sea39801991@gmail.com>
2019-07-25 12:21:07 +0800

    Add the RWD style into About.vue

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -18,7 +18,9 @@
         </nav>
 
         <main role="main" class="col-md-8 ml-sm-auto col-lg-8">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap">
+          <div class="d-flex justify-content-center flex-wrap flex-md-nowrap box">
+
+             <!-- The style of min-width: 768px -->
             <div class="left ">
               <img class="left-img mb-3" :src="img1" alt="photo">
               <p class="content-title mt-4 font">Advantage & Personality</p>
@@ -32,13 +34,47 @@
               
             </div>
 
-            <div class="right">
+            <div class="right ">
               <p class="pt-5 content-text font">
                 Hi, I am <strong class="name font">Hazel Pan</strong>, an easy-going individual. I can deal with colleagues harmoniously in most situations.
                 Always have a strong sense of curiosity and a desire to know how things work.
                 Love Traveling, painting and puzzle-solving.
               </p>
               <img class="right-img mt-5" :src="img2" alt="photo">
+            </div>
+
+            <!-- The style of max-width: 767px -->
+            <div class="left-sm">
+
+
+              <p class="content-title font mt-5">Hazel Pan</p>
+              <p class="content-text font">
+                "Life is a great big canvas, and you should throw all the paint on if you can"
+              </p>
+              <img class="left-img " :src="img1" alt="photo">
+              <p class="mt-5 content-text font">
+                Hi, I am <strong class="name font">Hazel Pan</strong>, an easy-going individual. I can deal with colleagues harmoniously in most situations.
+                Always have a strong sense of curiosity and a desire to know how things work.
+                Love Traveling, painting and puzzle-solving.
+              </p>
+            </div>
+
+            <div>
+              <hr>
+            </div>
+            
+
+            <div class="right-sm">
+              <img class="right-img mt-5" :src="img2" alt="photo">
+              <p class="content-title mt-5 font">Advantage & Personality</p>
+              
+                <ul class="content-text font align-items-center mt-3 ml-3 mr-3">
+                <li>Willing to innovate good ideas to help people.</li>
+                <li>Ability to work independently and within a team.</li>
+                <li>Willing to learn about new processes and concepts with a strong sense of curiosity.</li>
+                
+              </ul>
+              
             </div>
           </div>
         </main>
@@ -142,26 +178,58 @@
   [role="main"] {
      padding-top: 0%; /* Space for fixed navbar */
   }
-}
 
-.left,
+  .left,
 .right {
   float: left;
   width: 50%;
   padding: 0px;
 }
+
+
+
+.left-sm,
+.right-sm {
+  display: none;
+}
+
+
+
+}
+
+@media (max-width: 767px) {
+  .box{
+    text-align: center;
+  }
+    .left,
+    .right {
+      display: none;
+      float: center;
+  }
+.left-sm,
+.right-sm {
+  float: center;
+  width: 70%;
+}
+  
+
+  
+}
+
+
 .name{
   display: inline; 
-  font-size: 2vw;
+  font-size: 2em;
   font-weight: normal;
 }
 .content-title{
   text-align: center;
   padding: 0%;
-  font-size: 2vw;
+  font-size: 2em;
 }
 .content-text {
   padding: 0 3vw;
+  text-align: left;
 }
 
 .left-img,
@@ -172,6 +240,10 @@
   margin: 0px;
   padding: 0pt;
 }
+
+
+
+
 </style>
 
 <script>
@@ -179,7 +251,8 @@ export default {
   data() {
     return {
       img1: "static/images/head-1.png",
-      img2: "static/images/head-2.jpg"
+      img2: "static/images/head-2.jpg",
+      
     };
   }
 };
