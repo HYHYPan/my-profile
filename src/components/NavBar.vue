8a683129d33a11849bf0a37c8022b4f1d1bb5bda
HYHYPan <sea39801991@gmail.com>
2019-07-25 11:23:07 +0800

    Add the RWD style

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,16 +2,25 @@
   <div>
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top flex-md-nowrap p-1 shadow">
+        
         <img style="width:40px;" alt="logo" v-bind:src="logo">
         <a class="navbar-brand mr-auto" href="#" @click.prevent="getToPage('/')">Hazel Pan</a>
+        
+        <button type="button" class="menu-button">
+         三
+         
+        </button>
+        <input type="checkbox" class="menu-button-checkbox">
+        
         <ul class="nav justify-content-end">
-          <a class="nav-link nav-item" href="#" @click.prevent="getToPage('/about')">About</a>
-          <a class="nav-link nav-item" href="#" @click.prevent="getToPage('/education')">Education</a>
-          <a class="nav-link nav-item" href="#" @click.prevent="getToPage('/work')">Work experience</a>
-          <a class="nav-link nav-item" href="#" @click.prevent="getToPage('/skill')">Skills</a>
-          <a class="nav-link nav-item" href="#" @click.prevent="getToPage('/portfolio')">Portfolio</a>
-          <a class="nav-link nav-item" href="#" @click.prevent="getToPage('/login')">Login</a>
+          <li><a class="nav-link nav-item" href="#" @click.prevent="getToPage('/about')">About</a></li>
+          <li><a class="nav-link nav-item" href="#" @click.prevent="getToPage('/education')">Education</a></li>
+          <li><a class="nav-link nav-item" href="#" @click.prevent="getToPage('/work')">Work experience</a></li>
+          <li><a class="nav-link nav-item" href="#" @click.prevent="getToPage('/skill')">Skills</a></li>
+          <li><a class="nav-link nav-item" href="#" @click.prevent="getToPage('/portfolio')">Portfolio</a></li>
+          <li><a class="nav-link nav-item" href="#" @click.prevent="getToPage('/login')">Login</a></li>
         </ul>
+        
       </nav>
     </header>
 
@@ -22,6 +31,7 @@
 </template>
 
 <style lang="scss" scoped>
+
  .navbar{
    background: #6A7E91;
 }
@@ -30,6 +40,68 @@
   color: white;
   font-family: Noto, sans;
 }
+
+@media screen and (min-width: 1024px) {
+  .menu-button,
+  .menu-button-checkbox  {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 1023px) {
+
+  .menu-button {
+    float: right;
+    font-size: 25px;
+    line-height: 5vw;
+    color: white;
+    background: transparent;
+    padding: 0 15px;
+    border: 0;
+  }
+
+  .menu-button-checkbox {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 55px;
+    height: 40px;
+    opacity: 0;
+  }
+
+  .menu-button-checkbox:checked ~ .nav {
+    display: block;
+  }
+
+  // .navbar{
+  //   height: 40px;
+  // }
+  .nav {
+    display: none;
+    margin-top: 0;
+    padding-left: 0;
+    position: fixed;
+    right: 0;
+    top: 44px;
+    width: 150px;
+    height: 100%;
+    background: #2a4764;
+  }
+
+  .nav-item {
+    border: 0;
+    border-radius: 0;
+  }
+
+  .nav-item:hover, .nav-item:active {
+  background:#6A7E91;
+}
+
+  /*
+    往下繼續手機版 css code
+  */
+}
+
 
 </style>
 
